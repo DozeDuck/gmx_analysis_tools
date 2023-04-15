84a35528e7dceff80827ae563b692635f07520e9
Shangze <89918650+DozeDuck@users.noreply.github.com>
2023-04-15 11:28:38 +0100

    Add files via upload

---
+++ data_plotly/install_step.sh
@@ -0,0 +1,6 @@
+pip install plotly
+pip install -U kaleido
+conda install -c conda-forge pyinstaller
+# pip install --upgrade pyinstaller
+pyinstaller -F plotly_go.py -p /home/dozeduck/workspace/anaconda3/envs/plotly/lib/python3.11/site-packages
+
